3c6b2048afceafe7238c72c30d390293c0c0989a
Francis <bup9812@naver.com>
2020-07-03 23:56:21 +0900

    Update textMining.py

--- textMining.py
+++ textMining.py
@@ -1,115 +1,86 @@
-import copy
+from Bio import Entrez
 import math
 from collections import deque
 
+def probability(abstracts, keywords):
+    p = 0.0
+    count = 0.0
+    total = len(abstracts)
+
+    for words in abstracts:
+        has_terms = True
+        for t in keywords:
+            if not t in words:
+                has_terms = False
+        if has_terms:
+            count = count + 1
+
+    return count / total
+
 kegg_names = {}
 name_kegg = {}
-f1 = open('C:\\Users\Administrator\Desktop\genes.txt', 'r')
-for line in f1.readlines():
-    
+f = open('C:\\Users\wonde\Desktop\genes.txt', 'r')
+for line in f.readlines():
+
     t1 = line.split(';')[0]
     t2 = t1.split('\t')
     kegg_id = t2[0]
     kegg_names[kegg_id] = []
     for name in t2[1].split(','):
-        name = name.strip()
-        kegg_names[kegg_id].append(name)
-        name_kegg[name] = kegg_id
-f1.close()
-#print(name_kegg['CLCN5'])
+                name = name.strip()
+                kegg_names[kegg_id].append(name)
+                name_kegg[name] = kegg_id
 
-f2 = open('C:\\Users\Administrator\Desktop\\abstracts1.txt', 'r')
-data = f2.readlines()
+f.close()
+
+disease = 'Schizophrenia'.upper()
+
+print('Download abstracts...')
+
+Entrez.email = 'tkddudwhswk@naver.com'
+
+handle = Entrez.esearch(db='pubmed', term=disease, retmax=10000)
+record = Entrez.read(handle)
+
+downloaded_abstracts = []
+
+cnt = 0
+for pubmed_id in record['IdList']:
+    cnt = cnt + 1
+
+    print (cnt, '/', len(record['IdList']))
+    abstract = Entrez.efetch('pubmed', id=pubmed_id, retmode='text', rettype='abstracts').read()
+    downloaded_abstracts.append(abstract)
+
+keywords_in_abstract = []
+for ab in downloaded_abstracts:
+    keyword_box = []
+    words = ab.replace('.',' ').split(' ')
+    for w in words:
+        if w.upper() == disease:
+            keyword_box.append(w.upper())
+        else:
+            if w in name_kegg:
+                keyword_box.append(name_kegg[w])
+
+    keywords_in_abstract.append(keyword_box)
+
+print('Calculating MI....')
+
+scores = {}
+
+p_disease = probability(keywords_in_abstract, [disease])       #dent로 하면 결과 달라짐
+for kegg_id in kegg_names:
+    p_gene = probability(keywords_in_abstract, [kegg_id])
+    p_gene_disease = probability(keywords_in_abstract, [kegg_id, disease])
+
+    if p_gene !=0 and p_disease !=0 and p_gene_disease !=0:
+        mi = math.log2(p_gene_disease / (p_gene * p_disease))
+        scores[  kegg_names[kegg_id][0]  ] = mi
+
+f2 = open('C:\\Users\wonde\Desktop\\result.txt','w')
+
+for key in sorted(scores, key=scores.__getitem__, reverse=True):
+    f2.write(key + '\t' + str(scores[key]) + '\n')
+    print(key + '\t' + str(scores[key]))
 f2.close()
-
-print("Data",len(data))     #207248
-temp = []
-abstract = []
-for i in range(0, 207248):
-  temp.append(data[i])
-  if "PMID" in data[i]:
-    x = "".join(temp)
-    abstract.append(x)
-    #print("abstract", abstract)
-    temp = []
-
-
-file_data = "abstract-dentdiseas-set.txt"
-handle_file = open(file_data, "r")
-data = handle_file.readlines()
-handle_file.close()
-#print(len(data))  총 207248줄
-#print(data[27])
-
-documents = deque()
-temp = []
-abstract = []
-
-for i in range(0, 207248):
-  temp.append(data[i])
-  if "MEDLINE" in data[i]:
-    x = "".join(temp)
-    abstract.append(x)
-    #print("abstract", abstract)
-    temp = []
-
-#print(abstract[5])
-#print(len(abstract)) #9234
-
-
-def probability(data, target):
-  length = len(data)
-  count = 0
-  for i in range(0, length):
-    if target in data[i]:
-      count += 1
-  result = count/length
-  return result
-
-def probability2(data, target):
-      length = len(data)
-  count = 0
-  for i in range(0, length):
-    if target[0] in data[i]:
-      count += 1
-    elif target[1] in data[i]:
-      count += 1
-    elif target [2] in data[i]:
-      count += 1
-  result = count/length
-  return result
-
-def probability3(data, target1, target2):
-      length = len(data)
-  count = 0 
-  for i in range(0, length):
-    if target1 and target2[0] in data[i]:
-      count += 1
-    elif target1 and target2[1] in data[i]:
-      count += 1
-    elif target1 and target2[2] in data[i]:
-      count += 1
-  result = count/length
-  return result
-
-def main():
-      gene_list = ["CLCN5", "OCRL", "OCRL1"]
-  disease_probability = probability(abstract, "dent")
-  gene_probability = probability2(abstract, gene_list)
-  disease_gene_probability = probability3(abstract, "dent", gene_list)
-  p_value = math.log2(disease_gene_probability / (disease_probability * gene_probability))
-  
-  print("disease_probability :", disease_probability)
-  print("gene_probability :", gene_probability)
-  print("==================================================")  
-  print(gene_list[0], ":", gene_probability)
-  print(gene_list[1], ":", gene_probability)
-  print(gene_list[2], ":", gene_probability)
-  print("==================================================")
-  print("disease_gene_probability :", disease_gene_probability)
-  print("p_value :", p_value)
-
-if __name__ == "__main__":
-  main()
-
-"""
